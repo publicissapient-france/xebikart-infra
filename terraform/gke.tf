06133843943a9ceff6e7da70aa8da26d2f1fa81a
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-03-18 12:57:03 +0100

    Terraform/GKE: create cluster in appropriate project

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,6 +1,11 @@
 resource "google_container_cluster" "gke-cluster" {
-  name   = "xebikart-gke-1"
+  name   = "xebikart-gke-${var.environment}-1"
   region = "europe-west1"
-  project = "horgix-xebikart-1"
+  project = "${google_project.xebikart.project_id}"
   initial_node_count = 1
+
+  # Wait for GKE API to be enabled before trying to create the cluster
+  depends_on = [
+    "google_project_services.project_services"
+  ]
 }
