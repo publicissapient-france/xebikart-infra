821c9fd535a9754bb4c9f80998238868ef7f1ad6
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-04-09 18:59:36 +0200

    Terraform: add DNS Zone

---
+++ terraform/dns.tf
@@ -0,0 +1,10 @@
+resource "google_dns_managed_zone" "xebikart-zone" {
+  project = "${google_project.xebikart.project_id}"
+
+  name = "xebikart-zone"
+  dns_name = "xebik.art."
+  description = "XebiKart DNS Zone"
+  labels = {
+    project = "xebikart"
+  }
+}
