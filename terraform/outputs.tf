b71fb1547e7b8af445a4a802eb5b9bc3f87b9b7a
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-04-09 18:59:51 +0200

    Terraform: add DNS Zone nameservers to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "GKE Cluster region" {
 output "gcloud command to get GKE credentials" {
   value = "gcloud container clusters get-credentials ${google_container_cluster.gke-cluster.name} --region ${google_container_cluster.gke-cluster.region}"
 }
+
+output "DNS NameServers for the xebik.art zone" {
+  value = "${google_dns_managed_zone.xebikart-zone.name_servers}"
+}
