4e3f2fc7f15327cdb9d7271681bfe64e13b31bc3
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-04-10 09:52:13 +0200

    Terraform/GKE: add OAuth scope to nodes, including clouddns for external-dns

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -5,6 +5,21 @@ resource "google_container_cluster" "gke-cluster" {
   project = "${google_project.xebikart.project_id}"
   initial_node_count = 1
 
+
+  node_config {
+    oauth_scopes = [
+      # Defaults
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      # For External-DNS
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
+    ]
+  }
+
   # Wait for GKE API to be enabled before trying to create the cluster
   depends_on = [
     "google_project_services.project_services"
