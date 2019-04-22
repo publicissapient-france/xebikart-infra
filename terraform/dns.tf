56ffbfa3dfe47e998c0dfaf2fddc31629157a8e6
Alexis "Horgix" Chotard <Horgix@users.noreply.github.com>
2019-04-22 21:06:23 +0200

    Terraform: upgrade GCP provider from 2.1 to 2.5 and adapt resources accordingly (#23)
    * Terraform: bump GCP provider from 2.1 to 2.5
    * Terraform/DNS: add empty visibility due to a bug in gcp provider 2.4 - see comment
    * Terraform/GKE: use location instead of deprecated region property

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,6 +4,14 @@ resource "google_dns_managed_zone" "xebikart-zone" {
   name = "xebikart-zone"
   dns_name = "xebik.art."
   description = "XebiKart DNS Zone"
+
+  # Empty `visibility` is necessary in order to avoid unecessary diff.
+  # This is a bug since Terraform's GCP provider version 2.4 that has not been
+  # solved yet
+  # CHANGELOG mentionning it: https://github.com/terraform-providers/terraform-provider-google/blob/master/CHANGELOG.md#240-april-15-2019
+  # Issue about it: https://github.com/terraform-providers/terraform-provider-google/issues/3435
+  visibility = ""
+
   labels = {
     project = "xebikart"
   }
