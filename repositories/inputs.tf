15335781f9ab8c7ab208e3b75ac5143e99ec73e5
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-05-05 22:27:45 +0200

    Repos: add inputs with labels colors

---
+++ repositories/inputs.tf
@@ -0,0 +1,7 @@
+locals {
+  # XebiKart GitHub repositories label colors
+  label_color_component       = "83ea96"
+  label_color_kind            = "a2eeef"
+  label_color_scope           = "c5def5"
+  label_color_closing-reason  = "cfd3d7"
+}
