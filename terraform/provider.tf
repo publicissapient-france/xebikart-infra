ce941eb25f628117cf8c462fad72c2ec4c3f9645
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-05-06 13:19:47 +0200

    Terraform: add google-beta provider to be able to use Istio addon on GKE

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,6 +3,14 @@ provider "google" {
   version = "2.5.1"
 }
 
+# We need both the `google` and `google-beta` providers to use beta features
+# from GKE such as the Istio addon. See Terraform doc for more informations:
+# https://www.terraform.io/docs/providers/google/provider_versions.html
+provider "google-beta" {
+  region  = "europe-west1"
+  version = "2.5.1"
+}
+
 terraform {
   required_version = "~> 0.11"
   backend "gcs" {
