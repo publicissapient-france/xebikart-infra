2db14150861f58327b89c689046ebf2a4a7bb6fc
Alexis "Horgix" Chotard <Horgix@users.noreply.github.com>
2019-09-02 19:22:14 +0200

    Terraform/GKE: switch from default node pool to a separately managed one (#36)

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -9,26 +9,14 @@ resource "google_container_cluster" "gke-cluster" {
   name   = "xebikart-gke-${var.environment}-1"
   location = "europe-west1"
   project = "${google_project.xebikart.project_id}"
+
   min_master_version = "1.12.7-gke.7"
-  node_version = "1.12.7-gke.7"
+
+  remove_default_node_pool = true
   initial_node_count = 1
+
   monitoring_service = "monitoring.googleapis.com/kubernetes"
   logging_service = "logging.googleapis.com/kubernetes"
-
-
-  node_config {
-    oauth_scopes = [
-      # Defaults
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-      # For External-DNS
-      "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
-    ]
-  }
 
   addons_config {
     kubernetes_dashboard {
@@ -41,3 +29,44 @@ resource "google_container_cluster" "gke-cluster" {
     "google_project_services.project_services"
   ]
 }
+
+resource "google_container_node_pool" "main-nodes" {
+  name       = "main-node-pool"
+  location   = "europe-west1"
+  project = "${google_project.xebikart.project_id}"
+
+  cluster    = "${google_container_cluster.gke-cluster.name}"
+  initial_node_count = 1
+
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+
+  node_config {
+    #preemptible  = true
+    machine_type = "n1-standard-1"
+
+    #metadata = {
+    #  disable-legacy-endpoints = "true"
+    #}
+
+    oauth_scopes = [
+      # Defaults
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      # For External-DNS
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
+    ]
+  }
+}
+
