8eebd59c204cc5d097899f9133da876d93d03769
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-09-03 18:27:18 +0200

    Repos: add xebikart-links repository

--- repositories/repositories.tf
+++ repositories/repositories.tf
@@ -1,3 +1,19 @@
 data "github_repository" "xebikart-infra-repo" {
   full_name = "xebia-france/xebikart-infra"
 }
+
+resource "github_repository" "xebikart-links-repo" {
+  # Note : project will be Open Source by default
+  name        = "xebikart-links"
+  description = "XebiKart - Useful links webpage"
+  homepage_url = "http://links.xebik.art"
+
+  has_projects = false
+  has_wiki = false
+  allow_merge_commit = false
+  topics = [
+    "xebicon19",
+    "xebikart",
+    "links"
+  ]
+}
