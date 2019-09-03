d1c07a4a04124fab2bd13fbf9b73b61012649355
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-09-03 10:35:44 +0200

    Repos: refactor component labels into a wrapper module

--- repositories/inputs.tf
+++ repositories/inputs.tf
@@ -1,6 +1,5 @@
 locals {
   # XebiKart GitHub repositories label colors
-  label_color_component       = "83ea96"
   label_color_kind            = "a2eeef"
   label_color_scope           = "c5def5"
 }
