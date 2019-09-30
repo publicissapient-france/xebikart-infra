eafd46f9f87d302e08861d92dded43c3315f261c
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2019-09-30 14:06:38 +0200

    Repos/team-ar: remove old xebikart-android repo

--- repositories/repositories-team-ar.tf
+++ repositories/repositories-team-ar.tf
@@ -1,14 +1,3 @@
-# Android
-module "xebikart-android-github-repo" {
-  source = "./modules/xebikart-github-repo"
-
-  repo_name        = "android"
-  repo_description = "TODO"
-  repo_site_url    = "TODO"
-
-  additional_topics = ["TODO"]
-}
-
 # Android AR
 module "xebikart-android_ar-github-repo" {
   source = "./modules/xebikart-github-repo"
