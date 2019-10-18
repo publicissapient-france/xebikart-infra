dde53e8042e94be9653b01068adc6ff89b36dbd4
Nicolas Laille <nlaille@xebia.fr>
2019-10-18 13:00:53 +0200

    add ml-node-pool in terraform

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -70,3 +70,46 @@ resource "google_container_node_pool" "main-nodes" {
   }
 }
 
+resource "google_container_node_pool" "ml-nodes" {
+  name       = "ml-node-pool"
+  location   = "europe-west1"
+  project = "${google_project.xebikart.project_id}"
+
+  cluster    = "${google_container_cluster.gke-cluster.name}"
+  initial_node_count = 0
+
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 2
+  }
+
+  node_config {
+    #preemptible  = true
+    machine_type = "n1-standard-4"
+
+    #metadata = {
+    #  disable-legacy-endpoints = "true"
+    #}
+
+    taint {
+        key = "dedicated"
+        value = "ml"
+        effect = "NO_SCHEDULE"
+    }
+
+    oauth_scopes = [
+      # Defaults
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+    ]
+  }
+}
